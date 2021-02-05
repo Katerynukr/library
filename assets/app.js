077dcb825f979edf3635813034eb74e544c2736b
kateryna <kateryna_neholiuk@ukr.net>
2021-02-05 18:29:48 +0200

    style templeate

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import 'bootstrap';
 
 // start the Stimulus application
-import './bootstrap';
+// import './bootstrap';
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
+
